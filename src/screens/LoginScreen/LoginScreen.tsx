b0ca32f22f1a9963d7ed9088a63f377b32428916
Matt Oracz <contact@mattoracz.com>
2022-07-12 21:37:00 +0200

    Moved App to source folder and created simple LoginScreen

---
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, Text, useColorScheme } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const LoginScreen: React.FC<{}> = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text>LoginScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;

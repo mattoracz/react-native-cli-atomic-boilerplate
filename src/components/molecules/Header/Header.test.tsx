d560c6849fa5b800c8f5ef1052b3c3ba1c943805
Matt Oracz <contact@mattoracz.com>
2022-07-14 14:37:04 +0200

    Added few simple tests to molecules

--- src/components/molecules/Header/Header.test.tsx
+++ src/components/molecules/Header/Header.test.tsx
@@ -9,7 +9,20 @@ describe('Header', () => {
 
   it('should render correctly', () => {
     const tree = render(<Header>Header Text</Header>);
-
     expect(tree).toMatchSnapshot();
+  });
+
+  it('should render header', () => {
+    const component = <Header>Header Text</Header>;
+    const { getByTestId } = render(component);
+    const button = getByTestId('header');
+    expect(button).toBeDefined();
+  });
+
+  it('should render header text', () => {
+    const component = <Header>Header Text</Header>;
+    const { getByTestId } = render(component);
+    const buttonText = getByTestId('header-text');
+    expect(buttonText).toBeDefined();
   });
 });
--- src/components/molecules/Input/Input.test.tsx
+++ src/components/molecules/Input/Input.test.tsx
@@ -2,14 +2,42 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import { Input } from './Input';
 
-describe('Input', () => {
+describe('TextInput', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it('should render correctly', () => {
     const tree = render(<Input label="Primary Input" />);
-
     expect(tree).toMatchSnapshot();
+  });
+
+  it('should render input', () => {
+    const component = <Input label="Primary Input" />;
+    const { getByTestId } = render(component);
+    const input = getByTestId('input');
+    expect(input).toBeDefined();
+  });
+
+  it('should render correctly input', () => {
+    const testID = 'input';
+    const placeHolder = 'input placeholder';
+    const keyboardType = 'email-address';
+
+    const { getByTestId } = render(
+      <Input label={placeHolder} keyboardType={keyboardType} />,
+    );
+
+    const textInput = getByTestId(testID);
+    expect(textInput.props.placeholder).toEqual(placeHolder);
+    expect(textInput.props.keyboardType).toEqual(keyboardType);
+  });
+
+  it('should render input label', () => {
+    const label = 'Primary Input';
+    const component = <Input label={label} />;
+    const { getByText } = render(component);
+    const input = getByText(label);
+    expect(input).toBeDefined();
   });
 });
